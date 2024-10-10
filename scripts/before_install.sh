f5f58151f0d8ec8fd3ae221c3383ca99db78f50d
sungue1 <101311244+sungue1@users.noreply.github.com>
2024-10-10 13:42:53 +0900

    Delete scripts directory

--- scripts/before_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install any dependencies or clean previous installations
-
-# 예: 시스템 패키지 업데이트
-sudo yum update -y
-
-# 예: 기존 애플리케이션 파일 제거
-sudo rm -rf /opt/myapp/*
--- scripts/permissions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-chmod +x /opt/myapp/scripts/stop_application.sh
-chmod +x /opt/myapp/scripts/start_application.sh
-chmod +x /opt/myapp/scripts/before_install.sh
-chmod +x /opt/myapp/scripts/after_install.sh
