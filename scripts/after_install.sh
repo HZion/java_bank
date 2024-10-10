c5f4acb021338fd6366812b44e37d4f548b10ebe
sungue1 <101311244+sungue1@users.noreply.github.com>
2024-10-10 13:42:53 +0900

    Delete scripts directory

--- scripts/after_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Post-installation tasks
-
-# 예: 필요한 설정 파일 복사
-sudo cp /opt/myapp/config/default.conf /etc/myapp/
-
-# 예: 파일 권한 설정 (옵션)
-sudo chmod +x /opt/myapp/bank-0.0.1-SNAPSHOT.jar
