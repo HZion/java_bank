2a4f81d95016273eb7e19937c78b3b3718a089ac
wsg <sungue4098@naver.com>
2024-10-11 09:26:26 +0900

    sudo 제거

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Install any dependencies or clean previous installations
 
-# 예: 시스템 패키지 업데이트
-sudo yum update -y
+# 예: 시스템 패키지 업데이트 (sudo 제거)
+yum update -y
 
-# 예: 기존 애플리케이션 파일 제거
-sudo rm -rf /opt/myapp/*
+# 예: 기존 애플리케이션 파일 제거 (sudo 제거)
+rm -rf /opt/myapp/*
 
-sudo chmod 777 /opt/myapp/scripts/*.sh
+chmod 777 /opt/myapp/scripts/*.sh
