f1580d3899b3b9898ab9acc49f2ecc2b918bdb88
wsg <sungue4098@naver.com>
2024-10-11 09:42:38 +0900

    sh 수정

--- scripts/start_application.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Start the application
-
-# 예: 애플리케이션 실행 (백그라운드에서 실행)
-cd /opt/myapp
-nohup java -jar bank-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
-
-sudo chmod 777 /opt/myapp/scripts/*.sh
